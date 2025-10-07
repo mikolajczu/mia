5c73ca876659589063cfacd3bba42d84ca241ec3
mikolajczu <mikeyczu@gmail.com>
2025-10-07 13:03:22 +0200

    implement chat deletion functionality

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,6 @@
 class ChatsController < ApplicationController
+  before_action :authenticate_user!, only: %i[destroy]
+
   def show
     @chat = Chat.find(params[:id])
     @chats = current_user.chats.order(updated_at: :desc) if user_signed_in?
@@ -8,5 +10,16 @@ class ChatsController < ApplicationController
   def new
     @chat = Chats::CreateChatService.new(user: current_user, session: session).call
     redirect_to chat_path(@chat)
+  end
+
+  def destroy
+    @chat = current_user.chats.find(params[:id])
+    @chat.destroy
+
+    if current_user.chats.any?
+      redirect_to chat_path(current_user.chats.last)
+    else
+      redirect_to new_chat_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
-  resources :chats, only: %i[show new]
+  resources :chats, only: %i[show new destroy]
   resources :messages, only: %i[create]
 end
